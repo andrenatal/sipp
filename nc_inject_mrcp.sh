ce2bbbac14f10a405554827aa1c40ccb7c16030c
Andre Natal <anatal@gmail.com>
2016-03-03 14:17:57 -0300

    sipp

--- nc_inject_mrcp.sh
+++ nc_inject_mrcp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sess=${1:10}
 sh gen_mrcp_sess.sh $sess
-nc -w 3 192.168.3.2 1544  <  temp_session/$sess.sess
+cat temp_session/$sess.sess | nc -q 5 ${2} 1544  >  temp_session/$sess.output
