7c2ed0386ed9a5bcfb86fd19060103d5f7331bd5
Andre Natal <anatal@gmail.com>
2025-07-25 17:46:04 -0700

    make nc work in 2025

--- nc_inject_mrcp_asr.sh
+++ nc_inject_mrcp_asr.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sess=${1:10}
 sh gen_mrcp_sess_asr.sh $sess
-cat temp_session_asr/$sess.sess | nc -q 5 ${2} 1544 - >  temp_session_asr/$sess.output
+#cat temp_session_asr/$sess.sess | nc -q 5 ${2} 1544 - >  temp_session_asr/$sess.output
+nc -q 5 "$2" 1544 < temp_session_asr/"$sess".sess > temp_session_asr/"$sess".output
--- start_sipp_asr.sh
+++ start_sipp_asr.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #./sipp -sf uac_mrcp_asr.xml -ci 192.168.1.14 -i 192.168.1.14 -mi 192.168.1.14 -r 1 -rp 10000 192.168.1.10:8060
 #./sip352/sipp-3.5.2/sipp -sf uac_mrcp_asr.xml -ci 192.168.1.14 -i 192.168.1.14 -mi 192.168.1.14 -r 1 -rp 1 192.168.1.10:8060
-./sip352/sipp-3.5.2/sipp -sf uac_mrcp_asr.xml -ci 192.168.0.30 -i 192.168.0.30 -mi 192.168.0.30 -r 1 -rp 1 -m 1 -pause_msg_ign 192.168.0.41:8060
+./sip352/sipp-3.5.2/sipp -sf uac_mrcp_asr.xml -ci 192.168.191.200 -i 192.168.191.200 -mi 192.168.191.200 -r 1 -rp 1 -m 1 -pause_msg_ign 192.168.191.200:8060
